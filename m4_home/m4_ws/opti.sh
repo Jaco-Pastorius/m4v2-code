e6c5743e66bec858b110fcc41172128a1c49e6c5
Ioannis Mandralis <imandralis@gmail.com>
2024-01-31 11:47:38 -0800

    setup opti track network for CAST

--- m4_home/m4_ws/opti.sh
+++ m4_home/m4_ws/opti.sh
@@ -1,15 +1,2 @@
-echo "Creating two tmux sessions (vrpn_mocap, angle_broadcaster)"
-
-# Start two detached (-d) tmux sessions called vrpn_mocap and angle_broadcaster
-tmux new-session -d -s vrpn_mocap
-tmux new-session -d -s angle_broadcaster
-
-echo "Starting vrpn_client"
-
-tmux send-keys -t vrpn_mocap 'source /opt/ros/foxy/setup.bash' Enter
-tmux send-keys -t vrpn_mocap 'ros2 launch vrpn_mocap client.launch.yaml server:=192.168.0.33' Enter
-
-echo "Starting angle_broadcaster"
-
-tmux send-keys -t angle_broadcaster 'source install/setup.bash' Enter
-tmux send-keys -t angle_broadcaster 'ros2 run m4_base angle_broadcaster' Enter
+source /opt/ros/foxy/setup.bash 
+ros2 launch vrpn_mocap client.launch.yaml server:=192.168.0.108
