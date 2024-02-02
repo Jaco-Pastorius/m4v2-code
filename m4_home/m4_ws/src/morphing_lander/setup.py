8859aa3148bcee15f3a713b6792a0c00cfcabf51
Ioannis Mandralis <imandralis@gmail.com>
2024-02-02 11:12:46 -0800

    added morphing lander node logic

--- m4_home/m4_ws/src/morphing_lander/setup.py
+++ m4_home/m4_ws/src/morphing_lander/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+from glob import glob
+import os
 package_name = 'morphing_lander'
 
 setup(
@@ -10,16 +11,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='m4pc',
+    maintainer='root',
     maintainer_email='imandralis@gmail.com',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'offboard_control = morphing_lander.descent_controller:main',
+            'tilt_controller = morphing_lander.tilt_controller:main'
         ],
     },
 )
